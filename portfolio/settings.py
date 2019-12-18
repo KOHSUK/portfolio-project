9a659f2f8307316d31ef89a7604b6a59d3644ce5
Kosuke Kihara <ashenden1965@gmail.com>
2019-12-18 16:58:43 +0900

    deleted password

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': '226191',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
